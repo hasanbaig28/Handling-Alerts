02e84ff9e42bf16b0b9dacaed8a26e5d93556721
hasanbaig28 <93471313+hasanbaig28@users.noreply.github.com>
2021-11-04 15:06:46 +0530

    Add files via upload

---
+++ Alertpopup.java
@@ -0,0 +1,27 @@
+package auto;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alertpopup {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","E:\\browserdriver\\chrome D\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.indiehackers.com/sign-in");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		WebElement mail = driver.findElement(By.linkText("Reset"));
+		mail.click();
+		Thread.sleep(3000);
+		String word = driver.switchTo().alert().getText();
+		System.out.println("word");
+		driver.switchTo().alert().sendKeys("mohammedhasanbaig");
+		driver.switchTo().alert().accept();
+		Thread.sleep(1000);
+		driver.close();
+
+	}
+
+}
